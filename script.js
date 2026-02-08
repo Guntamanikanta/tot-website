5f0a8167339fa11e7d6f52c21c406a58fcd04799
Guntamanikanta <111900069+Guntamanikanta@users.noreply.github.com>
2026-02-08 19:01:42 +0530

    Add age verification functionality to script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-þÿ
+function enterSite() {
+  localStorage.setItem("ageVerified", "true");
+  document.getElementById("age-gate").style.display = "none";
+}
+
+window.onload = function () {
+  if (localStorage.getItem("ageVerified")) {
+    document.getElementById("age-gate").style.display = "none";
+  }
+};
